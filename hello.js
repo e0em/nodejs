2eb2dda1095cc2a635ad1326c33b92c823667964
marty <marty.chao@gmail.com>
2015-05-06 09:00:27 +0800

    OK, 改用了vim 取代了，nano 作爲commit editor

--- hello.js
+++ hello.js
@@ -1,5 +1,6 @@
 #!/usr/bin/nodejs 
 console.log('Hi World');
+console.log('The file name:');
 console.log(__filename);
 var args = process.argv.slice(2);
 console.log('Hello ' + args.join(' ') + '!');
